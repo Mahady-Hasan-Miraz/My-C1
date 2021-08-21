4c94234a9e6302f88f958f408effd4edd2ab71f9
Mahady Hasan Miraz <mhmiraz30@gmail.com>
2021-08-21 16:19:17 +0600

    rearranging and minor change

--- Marge Sort.c
+++ Marge Sort.c
@@ -1,6 +1,40 @@
-// Merge sort in C
+// Implementation of Merge sort Algorithm //
+
+/*  Mahady Hasan Miraz
+    ID: 1935202037  */
 
 #include <stdio.h>
+
+void main() {
+  int arr[50];/* = {6, 5, 12, 10, 9, 1};*/
+  int size;/* = sizeof(arr) / sizeof(arr[0]);*/
+
+  printf("How many elements do you want to sort? : ");
+  scanf("%d",&size);
+
+  printf("Enter %d elements : ",size);
+  for(int i=0;i<size;++i)
+  {
+    scanf("%d",&arr[i]);
+  }
+
+
+  mergeSort(arr, 0, size - 1);
+
+  printf("Sorted array: \n");
+  printArray(arr, size);
+}
+
+void mergeSort(int arr[], int l, int z) {
+  if (l < z) {
+
+    int m = (l+z) / 2;
+
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, z);
+    merge(arr, l, m, z);
+  }
+}
 
 void merge(int arr[], int x, int y, int z) {
 
@@ -43,30 +77,59 @@ void merge(int arr[], int x, int y, int z) {
   }
 }
 
-void mergeSort(int arr[], int l, int z) {
-  if (l < z) {
-
-    int m = l + (z - l) / 2;
-
-    mergeSort(arr, l, m);
-    mergeSort(arr, m + 1, z);
-
-    merge(arr, l, m, z);
-  }
-}
-
 void printArray(int arr[], int size) {
   for (int i = 0; i < size; i++)
     printf("%d ", arr[i]);
   printf("\n");
 }
 
-int main() {
-  int arr[] = {6, 5, 12, 10, 9, 1};
-  int size = sizeof(arr) / sizeof(arr[0]);
 
-  mergeSort(arr, 0, size - 1);
 
-  printf("Sorted array: \n");
-  printArray(arr, size);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* The algorithm is:
+Algorithm Merge(A,B,m,n)
+{
+     i=0, j=0, k=0;
+  while (i<m && j<n)    //2
+            {    if (A[i]<B[j])
+                       {                        
+                           C[k++]= A[i++];
+                        }
+                else
+                       {
+                           C[k++]= B[j++];
+                        }
+              }
+    for (; i<m; i++)
+               C[k++]= A[i];
+     for (; j<n; j++)
+               C[k++]= B[j];
+  }
+
+Algorithm MargeSort (A,l,h)
+{
+        if (l<h)
+            {
+                Mid = (l+h)/2;
+                MargeSort (A,l,Mid);
+               MargeSort (A, Mid+1, h);
+                Marge(A,l,mid,h)   ;
+            }
+*/
---
+++ avg.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a=1;
+    int b=8;
+    int c= (a+b)/2;
+    printf("ans= %d",c);
+}
