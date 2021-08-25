aedbce8334384e06a0b6c4856a65798b89f0c296
Mahady Hasan Miraz <61708989+Mahady-Hasan-Miraz@users.noreply.github.com>
2021-08-25 16:34:29 +0600

    final editiong of heap sort

--- Heap_Sort.c
+++ Heap_Sort.c
@@ -1,49 +1,22 @@
+// implementation of heap sort
 
-// Heap Sort in C
+    // Mahady Hasan Miraz
+    // ID: 1935202037
 
   #include <stdio.h>
 
-  int main() 
-  {
-    int arr[50],i,n,temp;
-    printf("How many elements to enter : ");
-    scanf("%d",&n);
-
-    printf("Enter %d elements : ",n);
-    for( i=0;i<n;++i)
-    {
-      scanf("%d",&arr[i]);
-    }
-
-    for ( i = n / 2 - 1; i >= 0; i--)
-      heapify(arr, n, i);
-
-    for ( i = n - 1; i >= 0; i--) 
-    {
-      temp = arr[0];
-      arr[0] = arr[i];
-      arr[i] = temp;
-      heapify(arr, i, 0);
-    }
-    
-    printf("Sorted array is :");
-    for ( i = 0; i < n; ++i)
-    printf("%d ", arr[i]);
-    printf("\n");
-  }
-
     void heapify(int arr[], int n, int i) 
   {
-    int large,R,L;
+    int large,left,right;       
     large = i;
-    L = 2*i + 1;
-    R = 2*i + 2;
+    left = 2*i + 1;
+    right = 2*i + 2;
 
-    if (R < n && arr[R] > arr[large])
-      large = R;
+    if (right < n && arr[right] > arr[large])
+      large = right;
 
-    if (L < n && arr[L] > arr[large])
-      large = L;
+    if (left < n && arr[left] > arr[large])
+      large = left;
 
     // Swap and continue heap if root not largest
     if (large != i) 
@@ -53,4 +26,38 @@
       arr[large] = temp;
       heapify(arr, n, large);
     }
+  }
+
+  int main() 
+  {
+    int arr[50],i,n,temp;
+
+    //arr[]={1,2,3,4,5,6};
+    //n= sizeof(arr)/sizeof(arr[0])
+
+    printf("How many elements to sort (less then 50) ? : ");
+    scanf("%d",&n);
+
+    printf("Enter %d elements : ",n);
+    for( i=0;i<n;++i)
+    {
+      scanf("%d",&arr[i]);
+    }
+
+    for ( i = n / 2 - 1; i >= 0; i--)//creat heap
+      heapify(arr, n, i);
+
+    for ( i = n - 1; i >= 0; i--) //delete element from heap & store
+    {
+      temp = arr[0];
+      arr[0] = arr[i];
+      arr[i] = temp;
+      heapify(arr, i, 0);
+    }
+    
+    printf("Sorted array is : ");
+    for ( i = 0; i < n; ++i)
+    printf("%d ", arr[i]);
+    printf("\n");
+    return 0;
   }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,13 +1,2 @@
-int main() {
-  int arr[] = {6, 5, 12, 10, 9, 1};
-  int size = sizeof(arr) / sizeof(arr[0]);
-  
-  printf("Original array\n");
-  display(arr, size);
-  
-  mergeSort(arr, 0, size - 1);
 
-  printf("Sorted array\n");
-  display(arr, size);
-  return 0;
-}
+    left = 2*i + 1;
