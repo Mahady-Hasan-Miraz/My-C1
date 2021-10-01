7c5e4a8c270d14e9ab51425f920e95635fe809ce
Mahady Hasan Miraz <61708989+Mahady-Hasan-Miraz@users.noreply.github.com>
2021-10-01 23:08:00 +0600

    changed variable names

--- Knapsack.c
+++ Knapsack.c
@@ -2,14 +2,14 @@
 
 void main()
 {
-   float profit;
-    int p[]={20,4,12,31,41,16},w[]={1,8,7,3,4,1},
+   float TotalProfit;
+    int profit[]={20,4,12,31,41,16},weight[]={1,8,7,3,4,1},
     n, no_items, cur_weight, item,i;
  
     printf("Enter the capacity of knapsack: ");
     scanf("%d", &n);
     
-    no_items = sizeof(w) / sizeof(w[0]);
+    no_items = sizeof(weight) / sizeof(weight[0]);
     int used[no_items];
 
     for (i = 0; i < no_items; ++i)
@@ -23,30 +23,47 @@ void main()
         item = 1;
         for (i = 0; i < no_items; ++i)
         {
-            if ((used[i] == 0) &&((item == 1) || ((float) p[i] / w[i] > (float) p[item] / w[item])))
+            if ((used[i] == 0) &&((item == 1) || ((float) profit[i] / weight[i] > (float) profit[item] / weight[item])))
             {
                 item = i;
             }
         }
 
         used[item] = 1;
-        float bb=cur_weight;       
-        cur_weight -= w[item];
+        float lweight=cur_weight;       
+        cur_weight -= weight[item];
 
-        profit += p[item];
+        TotalProfit += profit[item];
 
         if (cur_weight >= 0)
         {
-            printf("Object %d (Profit: %d_______Weight: %dKg). Space left: %d.\n", item + 1, p[item], w[item], cur_weight);
+            printf("Object %d (Profit: %d_______Weight: %dKg). Space left: %d.\n", item + 1, profit[item], weight[item], cur_weight);
         }
         else
         {
-            float lastP = (p[item])*(1 + (float) cur_weight / w[item]);
+            float lastP = (profit[item])*(1 + (float) cur_weight / weight[item]);
 
-            printf("Object %d (Profit: %.2f____Weight: %.2fKg). Space left: %d.\n", item + 1, lastP,bb, cur_weight+1);
-            profit -= p[item];
-            profit += lastP;
+            printf("Object %d (Profit: %.2f____Weight: %.2fKg). Space left: %d.\n", item + 1, lastP,lweight, cur_weight+1);
+            TotalProfit -= profit[item];
+            TotalProfit += lastP;
         }
     }
-    printf("Total profit %.2f \n", profit);
+    printf("Total profit %.2f \n", TotalProfit);
 }
+
+/*  algorithm 
+
+knapsack (weight[],profit[],corrent_weight)
+for(i=0 to size N)
+    do 
+     x[i]=0
+    weight=0
+    for i=0 to N
+        if weight + weight[i]<=weight
+            x[i]=1
+            weight=weight + weight[i]
+        else
+            x[i]=(weight - weight[i])/weight[i]
+            weight=weight[i]
+            break
+*/
