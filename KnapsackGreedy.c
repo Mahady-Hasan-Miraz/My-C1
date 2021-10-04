88da05274f78114cf5a442bf46ea4f14ff9f9d98
Mahady Hasan Miraz <61708989+Mahady-Hasan-Miraz@users.noreply.github.com>
2021-10-04 15:44:31 +0600

    changed variable and added limit

--- KnapsackGreedy.c
+++ KnapsackGreedy.c
@@ -3,11 +3,11 @@
 
 void main()
 {
-    int  item, i, used[100];;
-    float value[]={10,5,15,10,7,6,18,3},  weight[]={2,3,5,10,7,1,4,1};
+    int  item, i, used[101];;
+    float profit[]={10,5,15,10,7,6,18,3},  weight[]={2,3,5,10,7,1,4,1};
     float total_profit, no_items, total_weight, cur_weight, capacity,lastw,left;
 
-    printf("\n\t\tEnter the capacity of knapsack: ");
+    printf("\n\t\tEnter the capacity of knapsack(Max 100)): ");
     scanf("%f", &capacity);
     printf("----------------------------------------------------------------------\n");
 
@@ -19,12 +19,12 @@ void main()
     }
     if(total_weight<=capacity)
     {
-        no_items = sizeof(value) / sizeof(value[0]);
+        no_items = sizeof(profit) / sizeof(profit[0]);
         for(i=0; i<no_items; i++)
         {
-            total_profit+= value[i];
+            total_profit+= profit[i];
         }
-        printf("\t\tEvery object can be added.\n\t\tTotal profit:%.2f\n\t\tTotal weight: %.2f\n",total_profit,total_weight);
+        printf("\t\tEvery object can be added.\n\t\tTotal profit:%.2f\n",total_profit);
         printf("----------------------------------------------------------------------\n");
     }
     else
@@ -38,7 +38,7 @@ void main()
         item = -1;
         for (i = 0; i < no_items; ++i)
             if ((used[i] == 0) &&
-                ((item == -1) || ( value[i] / weight[i] >  value[item] / weight[item])))
+                ((item == -1) || ( profit[i] / weight[i] >  profit[item] / weight[item])))
                 item = i;
 
         used[item] = 1;
@@ -47,19 +47,19 @@ void main()
 
         cur_weight -= weight[item];
 
-        total_profit += value[item];
+        total_profit += profit[item];
         if (cur_weight >= 0)
-            printf("Added object %d    Profit: %.2f    Weight: %.2f Kg    Space left: %.2f.\n", item + 1, value[item], weight[item], cur_weight);
+            printf("Added object %d    Profit: %.2f    Weight: %.2f Kg    Space left: %.2f.\n", item + 1, profit[item], weight[item], cur_weight);
         else
         {
-            float item_percent =  ((1 +  cur_weight / weight[item]) * value[item]);
+            float item_percent =  ((1 +  cur_weight / weight[item]) * profit[item]);
             printf("Added object %d    Profit: %.2f    Weight: %.2f Kg    Space left: %.2f.\n", item + 1, item_percent, lastw, left);
-            total_profit -= value[item];
-            total_profit += (1 + cur_weight / weight[item]) * value[item];
+            total_profit -= profit[item];
+            total_profit += (1 + cur_weight / weight[item]) * profit[item];
         }
     }
     printf("----------------------------------------------------------------------\n");
 
-    printf("\t\tTotal profit: %.2f .\n\t\tTotal weight: %.2f\n", total_profit,total_weight);
+    printf("\t\tTotal profit: %.2f .\n", total_profit);
     }
 }
